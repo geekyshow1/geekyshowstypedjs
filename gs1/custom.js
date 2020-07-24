0579289593ad329d1016d7e630e2b32328ea02ff
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2020-07-24 13:41:58 +0530

    Add files via upload

---
+++ gs1/custom.js
@@ -0,0 +1,82 @@
+// Example 1
+let example1 = new Typed("#example1", {
+  stringsElement: "#mydivid",
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+// Example 2
+let example2 = new Typed("#example2", {
+  strings: ["Example 2", "Geeky Shows"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+// Example 3
+let example3 = new Typed("#example3", {
+  strings: ["Example 3", "Geeky Shows"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+// Example 4
+let example4 = new Typed("#example4", {
+  strings: ["Example 4", "Geeky Shows"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+  // fadeOut: true,
+  // fadeOutDelay: 2000,
+  // cursorChar: "_",
+  // startDelay: 2000,
+  // backDelay: 2000,
+  // showCursor: false,
+  // shuffle: true,
+  // smartBackspace: false,
+});
+
+// Example 5
+let example5 = new Typed("#example5", {
+  strings: ["You can write", "Your Name"],
+  typeSpeed: 100,
+  backSpeed: 10,
+  loop: true,
+  attr: "placeholder",
+});
+
+// Example 6
+let example6 = new Typed("#example6", {
+  strings: ["Example 6 ^3000", "Geeky Shows"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+// Example 7
+let example7 = new Typed("#example7", {
+  strings: ["I am Example 7 <br>Subscribe GeekyShows YT Channel"],
+  // strings: ["I am Example 7 ^3000 <br>Subscribe GeekyShows YT Channel"],
+  // strings: [
+  //   "I am Example 7 ^3000 <br>Subscribe GeekyShows ^3000 <br>YT Channel",
+  // ],
+  // strings: [
+  //   "I am Example 7 ^3000 `<br>Subscribe GeekyShows YT Channel`",
+  // ],
+  // strings: [
+  //   "I am Example 7 ^3000 `<br>Subscribe GeekyShows`^3000 `<br>YT Channel`",
+  // ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+// Example 8
+let example8 = new Typed("#example8", {
+  strings: ["Example 8", "Geeky Shows"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
---
+++ gs2/custom.js
@@ -0,0 +1,6 @@
+let spantyped = new Typed("#spanid", {
+  strings: ["Human", "Coder", "Developer", "Hacker", "Learner"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
